2378fc75a5a74c7251be1d1019c368f290a99075
sahanashettigar <r.sahana.shettigar@gmail.com>
2024-05-19 23:34:41 +0530

    changes to the API url

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -7,9 +7,12 @@ import { useRouter } from "next/navigation";
 const DeleteBlock = ({ id }) => {
   const router = useRouter();
   const deleteTicket = async () => {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `https://jiramandi.netlify.app//api/Tickets/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
     if (res.ok) {
       router.refresh();
     }
--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,9 +1,12 @@
 import TicketForm from "@/app/(components)/TicketForm";
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `https://jiramandi.netlify.app//api/Tickets/${id}`,
+      {
+        cache: "no-store",
+      }
+    );
     return res.json();
   } catch (error) {
     console.log(error);
